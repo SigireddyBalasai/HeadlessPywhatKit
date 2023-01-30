4ba3230d5fa652cc7e4e6600dd41b41cafd8215d
Shehroze Malik <shehroze@innidata.com>
2023-01-30 18:06:01 +0500

    argv fixed

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from HeadlessPywhatKit.sqsworker import process_sqs_que
-from HeadlessPywhatKit.app import create_app
 import sys
 
+print(sys.argv[1])
 #if args[1] is flask then run create app
 if sys.argv[1] == 'flask':
+    from HeadlessPywhatKit.app import create_app
+    
     app = create_app()
     app.run(host='0.0.0.0')
+
 if sys.argv[1] == 'sqs':
+    from HeadlessPywhatKit.sqsworker import process_sqs_que
     process_sqs_que()
 
